7c3dedb3ae9d9b9d6811f94cb2b6e1282b3712ba
Alex <alex@42layers.io>
2020-08-15 13:12:50 -0700

    remove old unused code

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
 
-// Matches with "/api/books"
+
+// TODO replace the below with relevant routes
+
+/*
 router.route("/")
   .get(booksController.findAll)
   .post(booksController.create);
@@ -12,5 +14,6 @@ router
   .get(booksController.findById)
   .put(booksController.update)
   .delete(booksController.remove);
+  */
 
 module.exports = router;
